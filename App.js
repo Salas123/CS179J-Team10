4213e0f55e68c15c5ef4c4eecbf63620fb1bf050
Miguel Rojas <mroja008@ucr.edu>
2020-04-26 16:09:36 -0700

    Resetting.

--- App.js
+++ App.js
@@ -1,57 +1,19 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {Header} from "react-native-elements";
-
 
 export default function App() {
-  return(
-      //Need fragment tags in order to use different stylings
-    <Fragment>
+  return (
     <View style={styles.container}>
-        <Header
-            leftComponent={{ icon: 'menu', color: '#fff' }}
-            centerComponent={{ text: 'Smart Tripod App', style: { color: '#fff' } }}
-            rightComponent={{ icon: 'home', color: '#fff' }}
-            containerStyle={styles.title}
-        />
-       <View style={styles.item1}>
-           <Text style={{fontSize:20, color:"#fff"}}>Camera Area</Text>
-       </View>
-       <View style={styles.item2}>
-           <Text style={{fontSize:20, color:"#fff"}}>Controls Area</Text>
-       </View>
-   </View>
-    </Fragment>
-
-);
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
- container: {
-     flex:1,
-     justifyContent:"center",
-     backgroundColor:"#fff",
-     alignItems:"stretch"
- },
- title: {
-     fontSize:20,
-     backgroundColor:"#029CAA",
-     margin: 0,
-     border: 0,
-     position: "relative"
-
- },
- item1: {
-     backgroundColor:"#89A6FB",
-     flex:1,
-     justifyContent: "center",
-     alignItems: "center"
- },
- item2: {
-     backgroundColor:"#f3e9d2",
-     flex:1,
-     justifyContent: "center",
-     alignItems: "center"
- },
-
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
